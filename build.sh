dbd1f436e3c2bd72edf7062227a0251794c64bc7
tukiyo3 <tukiyo3@gmail.com>
2019-11-29 09:57:04 +0900

    prefixの修正

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 export BUILD="docker build . --no-cache=false -f "
 
 # $BUILD build/Dockerfile -t tukiyo3/centos8-build
-$BUILD nkf/Dockerfile -t tukiyo3/centos8-build:local-nkf
-$BUILD w3m/Dockerfile -t tukiyo3/centos8-build:local-w3m
+
+# $BUILD nkf/Dockerfile -t tukiyo3/centos8-build:local-nkf
+# $BUILD w3m/Dockerfile -t tukiyo3/centos8-build:local-w3m
+# $BUILD tig/Dockerfile -t tukiyo3/centos8-build:local-tig
---
+++ tig/README.md
@@ -0,0 +1,4 @@
+## tig
+
+* https://github.com/jonas/tig/releases
+* https://github.com/jonas/tig/releases/download/tig-2.5.0/tig-2.5.0.tar.gz

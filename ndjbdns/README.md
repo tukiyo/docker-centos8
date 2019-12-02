dd9cc3f5afa5d970cd3e583565854fd64b8d98fa
tukiyo3 <tukiyo3@gmail.com>
2019-12-03 05:53:45 +0900

    add: ndjbdns

---
+++ ndjbdns/README.md
@@ -0,0 +1,5 @@
+# ndjbdns
+
+* http://pjp.dgplug.org/ndjbdns/
+* http://pjp.dgplug.org/ndjbdns/ndjbdns-1.06-1.fc20.src.rpm
+* http://pjp.dgplug.org/ndjbdns/ndjbdns-1.06.tar.gz
--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ IMAGE="tukiyo3/centos8-build:local-ranger"
 IMAGE="tukiyo3/centos8-build:local-autossh"
 IMAGE="tukiyo3/centos8-build:local-sshfs"
 IMAGE="tukiyo3/centos8-build:opt-eow"
+IMAGE="tukiyo3/centos8-build:opt-ndjbdns"
 
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
